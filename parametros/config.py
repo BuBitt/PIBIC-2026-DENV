74a281e92566da565e9504900deae7b033a748a1
Bruno <you@example.com>
2026-09-07 12:14:01 -0300

    Add: parametros universais

---
+++ parametros/config.py
@@ -0,0 +1,9 @@
+# T CELL CLASS 1
+parametro_de_filtragem_mbp_tcc1 = 5
+percent_match_minimo_tcc1 = 95.0
+filtarar_por_qte_de_alelos_tcc1 = 10
+
+# T CELL CLASS 2
+parametro_de_filtragem_mbp_tcc2 = 5
+percent_match_minimo_tcc2 = 95.0
+filtarar_por_qte_de_alelos_tcc2 = 10
