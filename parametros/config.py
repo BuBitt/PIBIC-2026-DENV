21f0d910e35c70eadc6ecdf1e61ce9c6fa37c1f6
Bruno <you@example.com>
2026-09-10 11:59:17 -0300

    Remove peptide sequences from peptideos.fasta, update filtering parameter for T Cell Class 2 in config.py, and note binary difference in compiled config.pyc.

--- parametros/config.py
+++ parametros/config.py
@@ -6,4 +6,4 @@ filtarar_por_qte_de_alelos_tcc1 = 10
 # T CELL CLASS 2
 parametro_de_filtragem_mbp_tcc2 = 5
 percent_match_minimo_tcc2 = 95.0
-filtarar_por_qte_de_alelos_tcc2 = 10
+filtarar_por_qte_de_alelos_tcc2 = 3
